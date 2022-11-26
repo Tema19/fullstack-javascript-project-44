81b44cc5f862c076d9da75a0818148a8d761998e
Tema <artemiyd90@gmail.com>
2022-11-26 20:31:03 +0500

    Change for "May I have your name?"

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export const greetings = () => {
-let name = readlineSync.question('What is your name?: ');
+let name = readlineSync.question('May I have your name?: ');
 console.log("Hello, "+ name);
 }
