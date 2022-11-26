489237b84a011889221f4e3024f2f471b245d695
Tema <artemiyd90@gmail.com>
2022-11-27 03:00:39 +0500

    wrong work of prime game, added new function

--- src/brain-prime.js
+++ src/brain-prime.js
@@ -1,19 +1,17 @@
 import readlineSync from 'readline-sync';
+import { prime } from "../src/prime-func.js"; 
 export function primeGame() {
     console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
     for(let i = 0; i < 3; i++){
-            const integer = Math.floor(Math.random() * 10);
-            const float = Math.random()
-            const array = [float, integer]
-            const number = array[Math.floor(Math.random() * 2)];
+            const number = Math.floor(Math.random() * 100);
             console.log('Question: '+ number)
             const answer = readlineSync.question('Your answer: ');
-            if (answer == 'yes'&& number == integer || answer == 'no'&& number == float ) {
+            if (answer == 'yes'&& prime(number) == true || answer == 'no'&& prime(number) == false ) {
                 console.log("Correct!");
-            } else if (answer == 'no'&& number == integer){
+            } else if (answer == 'no'&& prime(number) == false){
                 console.log("'no' is wrong answer ;(. Correct answer was 'yes'."); 
                 return false;
-            } else if (answer == 'yes'&& number == float){
+            } else if (answer == 'yes'&& prime(number) == true ){
                 console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
                 return false 
             } else {
