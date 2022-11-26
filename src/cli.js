bed00ec6fb3a63bb5eb5c7e72f65d592eea98b8b
Tema <artemiyd90@gmail.com>
2022-11-27 03:43:23 +0500

    Greetings fix

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default function greetings() {
+export default function greetings () {
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}`);
 }
