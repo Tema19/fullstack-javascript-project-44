43930f39c4dbb6e9f17a920daf6a1238c89cc943
Tema <artemiyd90@gmail.com>
2022-11-26 20:28:13 +0500

    Change for "May I have your name?"

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 export const game_3 = (Game) => {
-    const name = readlineSync.question('What is your name?: ');
+    const name = readlineSync.question('May I have your name? ');
     console.log("Hello, "+ name);
     if (Game() == false) 
     {return console.log("Let's try again, " + name + "!");}
