c1ca422b3f6821da848168564b14036aaaeab0c8
Tema <artemiyd90@gmail.com>
2022-11-26 21:06:17 +0500

    Error correction in prime Game

--- src/brain-prime.js
+++ src/brain-prime.js
@@ -2,20 +2,22 @@ import readlineSync from 'readline-sync';
 export function primeGame() {
     console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
     for(let i = 0; i < 3; i++){
-            const array = [Math.random(), Math.floor(Math.random() * 10)]
+            const integer = Math.floor(Math.random() * 10);
+            const float = Math.random()
+            const array = [float, integer]
             const number = array[Math.floor(Math.random() * 2)];
             console.log('Question: '+ number)
             const answer = readlineSync.question('Your answer: ');
-            if (answer == 'yes'&& number == Math.round(number)) {
+            if (answer == 'yes'&& number == integer || answer == 'no'&& number == float ) {
                 console.log("Correct!");
-            } else if (answer == 'no'&& number == Math.round(number)){
+            } else if (answer == 'no'&& number == integer){
                 console.log("'no' is wrong answer ;(. Correct answer was 'yes'."); 
                 return false;
-            } else if (answer == 'yes'&& number !== Math.round(number)){
+            } else if (answer == 'yes'&& number == float){
                 console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
                 return false 
             } else {
-                console.log("Correct!");
+                console.log("Wrong input!");
             }
         }
 }
